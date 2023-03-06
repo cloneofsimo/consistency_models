2fed8e921b6b32c965aeaef1cc64952d9eed64c9
Thomas Capelle <tcapelle@pm.me>
2023-03-06 19:38:12 +0000

    fmnist

--- consistency_models/utils.py
+++ consistency_models/utils.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 
 import torchvision.transforms as T
-from torchvision.datasets import MNIST, CIFAR10
+from torchvision.datasets import MNIST, CIFAR10, FashionMNIST
 
 
 
@@ -58,11 +58,30 @@ def cifar10_dl(batch_size=128, num_workers=4):
 
     return dataloader
 
+def fmnist_dl(batch_size=128, num_workers=4):
+    tf = T.Compose(
+            [
+                T.Pad(2),
+                T.ToTensor(),
+                T.Nromalize(0.5, 0.5),
+            ]
+        )
+    
+    dataset = FashionMNIST(
+            "./data",
+            train=True,
+            download=True,
+            transform=tf,
+            )
+
+    dataloader = DataLoader(datasetm, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
 def get_data(dataset="mnist", batch_size=128, num_workers=4):
     if dataset == "mnist":
         return mnist_dl(batch_size, num_workers)
     elif dataset == "cifar10":
         return cifar10_dl(batch_size, num_workers)
+    elif dataset == "fmnist":
+        return fmnist_dl(batch_size, num_workers)
     else:
         raise ValueError(f"Unknown dataset: {dataset}")
