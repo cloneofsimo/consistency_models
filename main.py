305f770e987b8f4485d4d2499a9fff4ce693707e
Simo Ryu <cloneofsimo@gmail.com>
2023-03-06 22:26:12 +0900

    cifar10

--- main.py
+++ main.py
@@ -9,17 +9,13 @@ import math
 import torch
 from torch.utils.data import DataLoader
 
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR10
 from torchvision import transforms
 from torchvision.utils import save_image, make_grid
 
 from consistency_models import ConsistencyModel, kerras_boundaries
 
-
-def train_mnist(n_epoch: int = 100, device="cuda:0"):
-    model = ConsistencyModel(1)
-    model.to(device)
-
+def mnist_dl():
     tf = transforms.Compose(
         [
             transforms.Pad(2),
@@ -36,17 +32,42 @@ def train_mnist(n_epoch: int = 100, device="cuda:0"):
     )
 
     dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=20)
+    
+    return dataloader
+
+def cifar10_dl():
+    tf = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    dataset = CIFAR10(
+        "./data",
+        train=True,
+        download=True,
+        transform=tf,
+    )
+
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=20)
+    
+    return dataloader
+
+def train(n_epoch: int = 100, device="cuda:0", dataloader= mnist_dl(), n_channels = 1, name = "mnist"):
+    model = ConsistencyModel(n_channels)
+    model.to(device)
     optim = torch.optim.AdamW(model.parameters(), lr=1e-4)
 
     # Define \theta_{-}, which is EMA of the params
-    ema_model = ConsistencyModel(1)
+    ema_model = ConsistencyModel(n_channels)
     ema_model.to(device)
     ema_model.load_state_dict(model.state_dict())
 
     for epoch in range(1, n_epoch):
         N = math.ceil(math.sqrt((epoch * (150**2 - 4) / n_epoch) + 4) - 1) + 1
         boundaries = kerras_boundaries(7.0, 0.002, N, 80.0).to(device)
-        print(boundaries)
+        
 
         pbar = tqdm(dataloader)
         loss_ema = None
@@ -81,25 +102,26 @@ def train_mnist(n_epoch: int = 100, device="cuda:0"):
         with torch.no_grad():
             # Sample 5 Steps
             xh = model.sample(
-                torch.randn(16, 1, 32, 32, device=device) * 80.0,
+                torch.randn_like(x).to(device = device) * 80.0,
                 list(reversed([5.0, 10.0, 20.0, 40.0, 80.0])),
             )
             xh = (xh * 0.5 + 0.5).clamp(0, 1)
             grid = make_grid(xh, nrow=4)
-            save_image(grid, f"./contents/ct_sample_5step_{epoch}.png")
+            save_image(grid, f"./contents/ct_{name}_sample_5step_{epoch}.png")
 
             # Sample 2 Steps
             xh = model.sample(
-                torch.randn(16, 1, 32, 32, device=device) * 80.0,
+                torch.randn_like(x).to(device = device) * 80.0,
                 list(reversed([2.0, 80.0])),
             )
             xh = (xh * 0.5 + 0.5).clamp(0, 1)
             grid = make_grid(xh, nrow=4)
-            save_image(grid, f"./contents/ct_sample_2step_{epoch}.png")
+            save_image(grid, f"./contents/ct_{name}_sample_2step_{epoch}.png")
 
             # save model
-            torch.save(model.state_dict(), f"./ct_mnist.pth")
+            torch.save(model.state_dict(), f"./ct_{name}.pth")
 
 
 if __name__ == "__main__":
-    train_mnist()
+    #train()
+    train(dataloader=cifar10_dl(), n_channels=3, name = "cifar10")
